01d6b75841501f3f624ee0b60b68229dac3d2772
Roman Coropat <roman.koropat@gmail.com>
2023-04-07 17:13:14 +0300

    fix: contacts btn

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export default function Hero() {
     return (
         <div className="text-gray-50 font-lexend flex flex-col items-center justify-center h-screen z-40 px-5 " >
@@ -6,11 +8,9 @@ export default function Hero() {
             <p className="text-yellow-500 max-w-4xl text-center pt-4 ">
                 Web developer passionate about creating elegant and functional web applications.
             </p>
-               <a href="#contacts" className="border border-yellow-500
-                py-2 px-5 mt-6 rounded-md hover:bg-yellow-500 hover:text-gray-900 
-                font-bold hover:shadow-lg 
-                active:bg-yellow-600 focus:outline-none focus:ring focus:ring-yellow-300
-                ">Get in touch</a>
+
+            <Link to="/contacts" className='btn-yellow'>Get in touch</Link>
+              
 
 
           
